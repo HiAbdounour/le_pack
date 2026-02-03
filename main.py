23bdbec034244d501e2e24b17ca2ba687f8d6e6b
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-02-03 12:59:44 +0100

    Create main.py with description of what to do

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+
+import noormiextracts # if really needed here !
+# import global vars (points,...) & global funcs (erase,blit,...)
+# pour tous les jeux : from XXX import main
+
+# ==== construction ====
+# display main window
+# buttons => each button : blit & launch it's own game function
+
+# for each game XXX : we have a main function that launches game
+# we'll need to take account of returning to main window (takccnt in XXX.main() or main()
+
